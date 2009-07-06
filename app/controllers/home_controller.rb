7fd15384c5d7b9e4ff5be381a70467bc957ce79e
Gilles Mathurin <gillesmath@me.com>
2009-07-06 09:29:04 -0400

    creation de la ressource Projet

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,17 @@ class HomeController < ApplicationController
   # GET 'home/accueil'
   def accueil
   end
+  
+  def service
+    
+  end
+  
+  def societe
+    
+  end
+  
+  def contact
+    
+  end
 
 end
