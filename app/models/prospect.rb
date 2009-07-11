3896eaab075784ec94fef990f0268f6bfe9e53f9
Gilles Mathurin <gillesmath@me.com>
2009-07-10 22:08:21 -0400

    mise en page de la page de contacts

--- app/models/prospect.rb
+++ app/models/prospect.rb
@@ -1,2 +1,5 @@
 class Prospect < ActiveRecord::Base
+  CATEGORY = [['site internet', 'site'], ["Systeme d'information", 'SI'],
+   ["WebService ou WebApplication","WebService ou WebApplication"],
+    ['Infographie & PAO', 'Infographie & PAO']]
 end
