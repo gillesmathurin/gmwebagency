afcda0b4fd643fe8375ef9b1abbf7bda18d18f2a
Gilles Mathurin <gillesmath@me.com>
2009-07-23 22:08:59 -0400

    tentative de preload du fond du menu

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,10 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+var preloadFlag = false;
+function preloadImages() {
+	if (document.images) {
+		bk_menuon = newImage("/images/bk_menuon.gif");
+		preloadFlag = true;
+	}
+}
