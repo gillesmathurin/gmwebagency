653bdae56f215060cf8d494b866d9ab0583a6166
Gilles MATHURIN <gillesmath@macmini.local>
2010-03-27 11:19:30 -0400

    Fixed the initializers/mail.rb

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,4 +1,4 @@
-Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)
+# Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.perform_deliveries = true
 ActionMailer::Base.raise_delivery_errors = true
@@ -8,8 +8,8 @@ ActionMailer::Base.smtp_settings = {
   :enable_starttls_auto => true, 
   :address              => "smtp.gmail.com", 
   :port                 => 587, 
-  :domain               => 'gmwebagency.com',
-  :authentication       => :login,
+  # :domain               => 'gmwebagency.com',
+  :authentication       => :plain,
   :user_name            => "gwadaman30@gmail.com", 
   :password             => "carmen30", 
 }
